34b979cfd7bd3c86911b925b3162caa455c0f6c2
Dmitry Ovcharov <mr.celestian@gmail.com>
2020-10-13 20:59:39 +0300

    BigRefactoring

--- AStarPathLibrary/AStarPathfinder.cs
+++ AStarPathLibrary/AStarPathfinder.cs
@@ -1,17 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 
 namespace AStarPathLibrary
 {
     public sealed partial class AStarPathfinder
     {
-        private const int NON_WALKABLE = 9;
+        private const int NonWalkable = 9;
         private readonly List<Node> _openList;
         private readonly List<Node> _closedList;
-        //private readonly List<Node> _fixedSizeAllocatorNodeList;
-        //private readonly List<Node> _mapSearchNodePoolList;
 
         private readonly Func<int, int, int> _getMapDelegate;
         private SearchState _searchState = SearchState.NotInitialized;
@@ -57,7 +54,7 @@ namespace AStarPathLibrary
             }
         }
 
-        private void SetStartAndGoalStates(Node startSearchNode, Node goalSearchNode)
+        private void SetStartAndGoalNode(Node startSearchNode, Node goalSearchNode)
         {
             _startNode = startSearchNode;
             _goalNode = goalSearchNode;
@@ -70,46 +67,30 @@ namespace AStarPathLibrary
             _openList.Add(_startNode);
         }
 
-        // Advances search one step
         private SearchState SearchStep()
         {
-            Debug.Assert((_searchState > SearchState.NotInitialized) && (_searchState < SearchState.Invalid));
-
-            // Next I want it to be safe to do a searchstep once the search has succeeded...
             if (_searchState == SearchState.Succeeded || _searchState == SearchState.Failed)
             {
                 return _searchState;
             }
 
-            // Failure is defined as emptying the open list as there is nothing left to
-            // search...
-            // New: Allow user abort
-            if (_openList.Count == 0)
+            if (!_openList.Any())
             {
                 FreeSolutionNodes();
                 _searchState = SearchState.Failed;
                 return _searchState;
             }
 
-            // Pop the best node (the one with the lowest f)
-            Node node = _openList[0]; // get pointer to the node
+            Node node = _openList[0];
             _openList.RemoveAt(0);
 
-            //System.Console.WriteLine("Checking node at " + n.m_UserState.position + ", f: " + n.f);
-
-            // Check for the goal, once we pop that we're done
             if (node.IsEqual(_goalNode))
             {
-                // The user is going to use the Goal Node he passed in
-                // so copy the parent pointer of n
                 _goalNode.Parent = node.Parent;
                 _goalNode.Cost = node.Cost;
 
-                // A special case is that the goal was passed in as the start state
-                // so handle that here
                 if (!node.IsEqual(_startNode))
                 {
-                    // set the child pointers in each node (except Goal which has no child)
                     Node nodeChild = _goalNode;
                     Node nodeParent = _goalNode.Parent;
 
@@ -119,178 +100,140 @@ namespace AStarPathLibrary
                         nodeChild = nodeParent;
                         nodeParent = nodeParent.Parent;
                     }
-                    while (nodeChild != _startNode); // Start is always the first node by definition
+                    while (nodeChild != _startNode);
                 }
-
-                // delete nodes that aren't needed for the solution
-                //FreeUnusedNodes();
 
                 _searchState = SearchState.Succeeded;
                 return _searchState;
             }
-            else // not goal
+
+            var successors = this.GetSuccessors(node).ToList();
+            bool ret = successors.Any();
+
+            if (!ret)
             {
-                var successors = this.GetSuccessors(node).ToList();
-                bool ret = successors.Any();
+                FreeSolutionNodes();
+                _searchState = SearchState.OutOfMemory;
+                return _searchState;
+            }
 
-                if (!ret)
-                {
-                    FreeSolutionNodes();
-                    _searchState = SearchState.OutOfMemory;
-                    return _searchState;
-                }
-
-                int successors_size = successors.Count;
+            int successorsSize = successors.Count;
                 
-                for (int i = 0; i < successors_size; ++i)
+            for (int i = 0; i < successorsSize; ++i)
+            {
+                Node successor = successors[i];
+                float cost = node.Cost + node.GetCost(successor);
+                Node openlistResult = null;
+                int openlistSize = _openList.Count;
+                bool foundOpenNode = false;
+                for (int j = 0; j < openlistSize; ++j)
                 {
-                    // Now handle each successor to the current node ...
-                    Node successor = successors[i];
-
-                    // 	The g value for this successor ...
-                    float newg = node.Cost + node.GetCost(successor);
-
-                    // Now we need to find whether the node is on the open or closed lists
-                    // If it is but the node that is already on them is better (lower g)
-                    // then we can forget about this successor
-
-                    // First linear search of open list to find node
-                    Node openlist_result = null;
-                    int openlist_size = _openList.Count;
-                    bool foundOpenNode = false;
-                    for (int j = 0; j < openlist_size; ++j)
+                    openlistResult = _openList[j];
+                    if (openlistResult.IsEqual(successor))
                     {
-                        openlist_result = _openList[j];
-                        if (openlist_result.IsEqual(successor))
-                        {
-                            foundOpenNode = true;
-                            break;
-                        }
+                        foundOpenNode = true;
+                        break;
                     }
-
-                    if (foundOpenNode)
-                    {
-                        // we found this state on open
-                        if (openlist_result.Cost <= newg)
-                        {
-                            // the one on Open is cheaper than this one
-                            continue;
-                        }
-                    }
-
-                    Node closedlist_result = null;
-                    int closedlist_size = _closedList.Count;
-                    bool foundClosedNode = false;
-                    for (int k = 0; k < closedlist_size; ++k)
-                    {
-                        closedlist_result = _closedList[k];
-                        if (closedlist_result.IsEqual(successor))
-                        {
-                            foundClosedNode = true;
-                            break;
-                        }
-                    }
-
-                    if (foundClosedNode)
-                    {
-                        // we found this state on closed
-                        if (closedlist_result.Cost <= newg)
-                        {
-                            // the one on Closed is cheaper than this one
-                            continue;
-                        }
-                    }
-
-                    // This node is the best node so far with this particular state
-                    // so lets keep it and set up its AStar specific data ...
-                    successor.Parent = node;
-                    successor.Cost = newg;
-                    successor.Distance = successor.CalculateDistance(_goalNode);
-                    successor.DistanceCostSum = successor.Cost + successor.Distance;
-
-                    // Remove successor from closed if it was on it
-                    if (foundClosedNode)
-                    {
-                        // remove it from Closed
-                        _closedList.Remove(closedlist_result);
-                    }
-
-                    // Update old version of this node
-                    if (foundOpenNode)
-                    {
-                        _openList.Remove(openlist_result);
-                    }
-
-                    SortedAddToOpenList(successor);
                 }
 
-                // push n onto Closed, as we have expanded it now
-                _closedList.Add(node);
-
-                if (_closedList.Count > _closedListHighWaterMark)
+                if (foundOpenNode)
                 {
-                    _closedListHighWaterMark = _closedList.Count;
+                    if (openlistResult.Cost <= cost)
+                    {
+                        continue;
+                    }
                 }
-            } // end else (not goal so expand)
 
-            return _searchState; // 'Succeeded' bool is false at this point.
+                Node closedlistResult = null;
+                int closedlistSize = _closedList.Count;
+                bool foundClosedNode = false;
+                for (int k = 0; k < closedlistSize; ++k)
+                {
+                    closedlistResult = _closedList[k];
+                    if (closedlistResult.IsEqual(successor))
+                    {
+                        foundClosedNode = true;
+                        break;
+                    }
+                }
+
+                if (foundClosedNode)
+                {
+                    if (closedlistResult.Cost <= cost)
+                    {
+                        continue;
+                    }
+                }
+
+                successor.Parent = node;
+                successor.Cost = cost;
+                successor.Distance = successor.CalculateDistance(_goalNode);
+                successor.DistanceCostSum = successor.Cost + successor.Distance;
+
+                if (foundClosedNode)
+                {
+                    _closedList.Remove(closedlistResult);
+                }
+
+                if (foundOpenNode)
+                {
+                    _openList.Remove(openlistResult);
+                }
+                SortedAddToOpenList(successor);
+            }
+            _closedList.Add(node);
+            if (_closedList.Count > _closedListHighWaterMark)
+            {
+                _closedListHighWaterMark = _closedList.Count;
+            }
+            return _searchState;
         }
 
         private IEnumerable<Node> GetSuccessors(Node node)
         {
             var result = new List<Node>();
-            NodePosition parentPos = node.Parent == null ? new NodePosition(0, 0) : node.Parent.Position;
-
-            result.Add(AddNeighbourNode(-1, 0, parentPos, node));
-            result.Add(AddNeighbourNode(0, -1, parentPos, node));
-            result.Add(AddNeighbourNode(1, 0, parentPos, node));
-            result.Add(AddNeighbourNode(0, 1, parentPos, node));
-
-            return result.Where( o => o != null); //TODO
+            var parentPos = node.Parent?.Position ?? new NodePosition(0, 0);
+            foreach ((int xOffset, int yOffset) in new []{(-1,0), (0,-1), (1,0), (0,1)})
+            {
+                if (IsNeighbourValid(xOffset, yOffset, parentPos, node))
+                {
+                   var successors = CreateNeighbourNode(xOffset, yOffset, node);
+                   result.Add(successors);  
+                }
+            }
+            return result;
         }
 
-        public bool IsNeigbourValid(NodePosition node, int xOffset, int yOffset) =>
-                this._getMapDelegate(node.X + xOffset, node.Y + yOffset) < NON_WALKABLE;
+        private bool IsNeighbourWalkable(NodePosition node, int xOffset, int yOffset) =>
+                this._getMapDelegate(node.X + xOffset, node.Y + yOffset) < NonWalkable;
 
-        private Node AddNeighbourNode(int xOffset, int yOffset, NodePosition parentPos, Node node)
+        private Node CreateNeighbourNode(int xOffset, int yOffset, Node node)
         {
-            Node newNode = null;
-            bool isNeigbourValid = IsNeigbourValid(node.Position, xOffset, yOffset);
-            if (isNeigbourValid &&
-                !(parentPos.X == node.Position.X + xOffset && parentPos.Y == node.Position.Y + yOffset))
-            {
-                (int, int) neighbourPos = (node.Position.X + xOffset, node.Position.Y + yOffset);
-                newNode = new Node(neighbourPos, this, _getMapDelegate);
-            }
+            (int, int) neighbourPos = (node.Position.X + xOffset, node.Position.Y + yOffset);
+            var newNode = new Node(neighbourPos, this, _getMapDelegate);
             return newNode;
         }
 
-        // Get start node
+        private bool IsNeighbourValid(int xOffset, int yOffset, NodePosition parentPos, Node node)
+        {
+            bool isNeighbourValid = IsNeighbourWalkable(node.Position, xOffset, yOffset);
+            return (isNeighbourValid &&
+                    !(parentPos.X == node.Position.X + xOffset && parentPos.Y == node.Position.Y + yOffset));
+        }
+
         private Node GetSolutionStart()
         {
             _currentSolutionNode = _startNode;
             return _startNode;
         }
 
-        // Get next node
         private Node GetSolutionNext()
         {
-            if (_currentSolutionNode != null)
-            {
-                if (_currentSolutionNode.Child != null)
-                {
-                    Node child = _currentSolutionNode.Child;
-                    _currentSolutionNode = _currentSolutionNode.Child;
-                    return child;
-                }
-            }
-            return null;
+            if (_currentSolutionNode?.Child == null) return null;
+            _currentSolutionNode = _currentSolutionNode.Child;
+            return _currentSolutionNode;
         }
 
-
-        // Free the solution nodes
-        // This is done to clean up all used Node memory when you are done with the
-        // search
         private void FreeSolutionNodes()
         {
             _openList.Clear();
@@ -302,54 +245,35 @@ namespace AStarPathLibrary
             var nodeStart = new Node(startPoint, this, _getMapDelegate);
             var nodeEnd = new Node(goalPoint, this, _getMapDelegate);
 
-            // Set Start and goal states
-            this.SetStartAndGoalStates(nodeStart, nodeEnd);
-
-            // Set state to Searching and perform the search
-            AStarPathfinder.SearchState searchState = SearchState.Searching;
-            uint searchSteps = 0;
+            this.SetStartAndGoalNode(nodeStart, nodeEnd);
+            SearchState searchState = SearchState.Searching;
 
             do
             {
                 searchState = this.SearchStep();
-                searchSteps++;
             }
             while (searchState == SearchState.Searching);
 
-            // Search complete
-            bool pathfindSucceeded = searchState == SearchState.Succeeded;
+            bool pathfinderSucceeded = searchState == SearchState.Succeeded;
 
             var newPath = new List<NodePosition>();
 
-            if (pathfindSucceeded)
+            if (pathfinderSucceeded)
             {
-                // Success
-                int numSolutionNodes = 0;	// Don't count the starting cell in the path length
-
-                // Get the start node
                 Node node = this.GetSolutionStart();
                 newPath.Add(node.Position);
-                ++numSolutionNodes;
-
-                // Get all remaining solution nodes
-                for (; ; )
+                for (;;)
                 {
                     node = this.GetSolutionNext();
-
                     if (node == null)
                     {
                         break;
                     }
-
-                    ++numSolutionNodes;
                     newPath.Add(node.Position);
                 };
-
-                // Once you're done with the solution we can free the nodes up
                 this.FreeSolutionNodes();
             }
             return newPath;
         }
     }
 }
-
